670eadd02144d578450bab01c24b2e2a7c644019
jinbeom hong <jinbeomhong@gmail.com>
2022-07-12 20:10:21 +0900

    edit 1004

--- 1004.cpp
+++ 1004.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <tuple>
-#include <variant>
 #include <cmath>
 
 using namespace std;
@@ -18,18 +15,13 @@ int main() {
 
         int n;
         cin >> n;
-        vector<tuple<int, int, int>> planets;
         while (n--) {
             int cx, cy, r;
             cin >> cx >> cy >> r;
 
-            planets.push_back({cx, cy, r});
-        }
-
-        for (auto p : planets) {
-            bool in_first = pow(x1 - get<0>(p), 2) + pow(y1 - get<1>(p), 2) < pow(get<2>(p), 2);
-            bool in_second = pow(x2 - get<0>(p), 2) + pow(y2 - get<1>(p), 2) < pow(get<2>(p), 2);
-
+            bool in_first = pow(x1 - cx, 2) + pow(y1 - cy, 2) < pow(r, 2);
+            bool in_second = pow(x2 - cx, 2) + pow(y2 - cy, 2) < pow(r, 2);
+            
             if ((in_first && !in_second) || (!in_first && in_second))
                 ++count;
         }
